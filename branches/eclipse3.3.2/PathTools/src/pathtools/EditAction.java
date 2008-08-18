17f4df105668adfe676412d7cc13d982403de672
sandipchitale <sandipchitale@fd0683c2-ad54-0410-998d-8999794ab908>
2008-08-18 03:38:13 +0000

    branching for eclipse3.3.2

---
+++ branches/eclipse3.3.2/PathTools/src/pathtools/EditAction.java
@@ -0,0 +1,98 @@
+package pathtools;
+
+import java.io.File;
+import java.text.MessageFormat;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+/**
+ * This launches the external text editor for selected folder or file.
+ * 
+ * @author Sandip V. Chitale
+ * 
+ */
+public class EditAction implements IWorkbenchWindowActionDelegate {
+	private File fileObject;
+
+	private static String fileEditComand = null;
+	private static String folderEditComand = null;
+
+	public void dispose() {
+	}
+
+	public void init(IWorkbenchWindow window) {
+
+	}
+
+	public void run(IAction action) {
+		// Get the configured explorer commands for folder and file
+		folderEditComand = Activator.getDefault().getPreferenceStore()
+				.getString(Activator.FOLDER_EDIT_COMMAND_KEY);
+		fileEditComand = Activator.getDefault().getPreferenceStore().getString(
+				Activator.FILE_EDIT_COMMAND_KEY);
+		if (fileEditComand == null || folderEditComand == null) {
+			return;
+		}
+		// Is this a physical file on the disk ?
+		if (fileObject != null) {
+			String commandFormat = fileObject.isDirectory() ? folderEditComand
+					: fileEditComand;
+
+			// Substitute parameter values ad format the explore command
+			String command = MessageFormat.format(Utilities
+					.convertParameters(commandFormat), new Object[] {
+					fileObject.getAbsolutePath().replace('/',
+							File.separatorChar).replace('\\',
+							File.separatorChar),
+					fileObject.getParentFile().getAbsolutePath().replace('/',
+							File.separatorChar).replace('\\',
+							File.separatorChar),
+					fileObject.getAbsolutePath().replace('\\', '/'),
+					fileObject.getParentFile().getAbsolutePath().replace('\\',
+							'/'),
+					fileObject.getAbsolutePath().replace('/', '\\'),
+					fileObject.getParentFile().getAbsolutePath().replace('/',
+							'\\'), });
+			// Launch the explore command
+			CommandLauncher.launch(command);
+		}
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		fileObject = null;
+		action.setEnabled(false);
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+			IPath location = null;
+			// Is only one item selected?
+			if (structuredSelection.size() == 1) {
+				Object firstElement = structuredSelection.getFirstElement();
+				if (firstElement instanceof IResource) {
+					// Is this an IResource
+					IResource resource = (IResource) firstElement;
+					location = resource.getLocation();
+				} else if (firstElement instanceof IAdaptable) {
+					IAdaptable adaptable = (IAdaptable) firstElement;
+					// Is this an IResource adaptable
+					IResource resource = (IResource) adaptable
+							.getAdapter(IResource.class);
+					if (resource != null) {
+						location = resource.getLocation();
+					}
+				}
+			}
+			if (location != null) {
+				fileObject = location.toFile();
+			}
+		}
+		action.setEnabled(fileObject != null);
+	}
+
+}
---
+++ branches/eclipse3.3.2/PathTools/src/pathtools/WorkbenchPreferencePage.java
@@ -0,0 +1,74 @@
+package pathtools;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+/**
+ * 
+ * This implements the preferences page using the FieldEditor.
+ * 
+ * @author Sandip V. Chitale
+ * 
+ */
+public class WorkbenchPreferencePage extends FieldEditorPreferencePage
+		implements IWorkbenchPreferencePage {
+
+	public WorkbenchPreferencePage() {
+		super(FieldEditorPreferencePage.GRID);
+	}
+
+	public void init(IWorkbench workbench) {
+		// Initialize the preference store we wish to use
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+	}
+
+	@Override
+	public String getDescription() {
+		return "Specify the commands for exploring folders and fies. You can\n"
+				+ "use \"\" (quotes) around command arguments with spaces in their value.\n"
+				+ "You can use the following parameters in the commands:\n\n"
+				+ Utilities.FILE_PATH
+				+ "  - path of the selected object with default file separator.\n"
+				+ Utilities.FILE_PARENT_PATH
+				+ "  - path of the parent of selected object with default file separator.\n"
+				+ Utilities.FILE_PATH_SLASHES
+				+ "  - path of the selected object with / file separator.\n"
+				+ Utilities.FILE_PARENT_PATH_SLASHES
+				+ "  - path of the parent of selected object with / file separator.\n"
+				+ Utilities.FILE_PATH_BACKSLASHES
+				+ "  - path of the selected object with \\ File separator.\n"
+				+ Utilities.FILE_PARENT_PATH_BACKSLASHES
+				+ "{parent-path-backslashes}  - path of the parent of selected object with \\ file separator.\n"
+				+ "\n";
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		// Folder explore command field
+		StringFieldEditor folderExploreCommad = new StringFieldEditor(
+				Activator.FOLDER_EXPLORE_COMMAND_KEY, "Explore Folder:",
+				getFieldEditorParent());
+		addField(folderExploreCommad);
+
+		// File explore command field
+		StringFieldEditor fileExploreCommad = new StringFieldEditor(
+				Activator.FILE_EXPLORE_COMMAND_KEY, "Explore File:",
+				getFieldEditorParent());
+		addField(fileExploreCommad);
+
+		// Folder editor command field
+		StringFieldEditor folderEditCommad = new StringFieldEditor(
+				Activator.FOLDER_EDIT_COMMAND_KEY, "Edit Folder:",
+				getFieldEditorParent());
+		addField(folderEditCommad);
+
+		// File editor command field
+		StringFieldEditor fileEditCommad = new StringFieldEditor(
+				Activator.FILE_EDIT_COMMAND_KEY, "Edit File:",
+				getFieldEditorParent());
+		addField(fileEditCommad);
+	}
+
+}
