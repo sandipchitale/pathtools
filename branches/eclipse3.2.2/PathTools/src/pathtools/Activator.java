e27057c83de3f887452c0023c48d27884cba7ffe
sandipchitale <sandipchitale@fd0683c2-ad54-0410-998d-8999794ab908>
2008-08-24 05:04:26 +0000

    Branching for Eclipse 3.2.2

---
+++ branches/eclipse3.2.2/PathTools/src/pathtools/Activator.java
@@ -0,0 +1,153 @@
+package pathtools;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ * 
+ * @author Sandip V. Chitale
+ * 
+ */
+public class Activator extends AbstractUIPlugin {
+	static final String FOLDER_EXPLORE_COMMAND_KEY = "folderExploreCommand";
+	static final String FILE_EXPLORE_COMMAND_KEY = "fileExploreCommand";
+
+	static String defaultFolderExploreCommand = "";
+	static String defaultFileExploreCommand = "";
+
+	static final String FOLDER_EDIT_COMMAND_KEY = "folderEditCommand";
+	static final String FILE_EDIT_COMMAND_KEY = "fileEditCommand";
+
+	static String defaultFolderEditCommand = "";
+	static String defaultFileEditCommand = "";
+
+	static {
+		if (Platform.OS_MACOSX.equals(Platform.getOS())) {
+			defaultFolderExploreCommand = "/usr/bin/open -a /System/Library/CoreServices/Finder.app \""
+					+ Utilities.FILE_PATH + "\"";
+			defaultFileExploreCommand = "/usr/bin/open -a /System/Library/CoreServices/Finder.app \""
+					+ Utilities.FILE_PARENT_PATH + "\"";
+			defaultFolderEditCommand = "/usr/bin/open -a /System/Library/CoreServices/Finder.app \""
+					+ Utilities.FILE_PATH + "\"";
+			defaultFileEditCommand = "/usr/bin/open -a /Applications/TextEdit.app \""
+					+ Utilities.FILE_PATH + "\"";
+		} else if (Platform.OS_WIN32.equals(Platform.getOS())) {
+			defaultFolderExploreCommand = "cmd /C start explorer /select,/e,\""
+					+ Utilities.FILE_PATH + "\"";
+			defaultFileExploreCommand = "cmd /C start explorer /select,/e,\""
+					+ Utilities.FILE_PATH + "\"";
+			defaultFolderEditCommand = "cmd /C start explorer /select,/e,\""
+					+ Utilities.FILE_PATH + "\"";
+			defaultFileEditCommand = "cmd /C start notepad \""
+					+ Utilities.FILE_PATH + "\"";
+		} else if (Platform.OS_LINUX.equals(Platform.getOS())) {
+			if (new File("/usr/bin/konqueror").exists()) {
+				defaultFolderExploreCommand = "/usr/bin/konqueror \""
+						+ Utilities.FILE_PATH + "\"";
+				defaultFileExploreCommand = "/usr/bin/konqueror \""
+						+ Utilities.FILE_PARENT_PATH + "\"";
+				defaultFolderEditCommand = "/usr/bin/konqueror \""
+						+ Utilities.FILE_PATH + "\"";
+			} else if (new File("/usr/bin/nautilus").exists()) {
+				defaultFolderExploreCommand = "/usr/bin/nautilus \""
+						+ Utilities.FILE_PATH + "\"";
+				defaultFileExploreCommand = "/usr/bin/nautilus \""
+						+ Utilities.FILE_PARENT_PATH + "\"";
+				defaultFolderEditCommand = "/usr/bin/nautilus \""
+						+ Utilities.FILE_PATH + "\"";
+			}
+			if (new File("/usr/bin/kedit").exists()) {
+				defaultFileEditCommand = "/usr/bin/kedit \""
+						+ Utilities.FILE_PATH + "\"";
+			} else if (new File("/usr/bin/gedit").exists()) {
+				defaultFileEditCommand = "/usr/bin/gedit \""
+						+ Utilities.FILE_PATH + "\"";
+			}
+		} else if (Platform.OS_SOLARIS.equals(Platform.getOS())) {
+			if (new File("/usr/bin/konqueror").exists()) {
+				defaultFolderExploreCommand = "/usr/bin/konqueror \""
+						+ Utilities.FILE_PATH + "\"";
+				defaultFileExploreCommand = "/usr/bin/konqueror \""
+						+ Utilities.FILE_PARENT_PATH + "\"";
+				defaultFolderEditCommand = "/usr/bin/konqueror \""
+						+ Utilities.FILE_PATH + "\"";
+			} else if (new File("/usr/bin/nautilus").exists()) {
+				defaultFolderExploreCommand = "/usr/bin/nautilus \""
+						+ Utilities.FILE_PATH + "\"";
+				defaultFileExploreCommand = "/usr/bin/nautilus \""
+						+ Utilities.FILE_PARENT_PATH + "\"";
+				defaultFolderEditCommand = "/usr/bin/nautilus \""
+						+ Utilities.FILE_PATH + "\"";
+			} else {
+				defaultFolderExploreCommand = "filemgr -c -d \""
+						+ Utilities.FILE_PATH + "\"";
+				defaultFolderExploreCommand = "filemgr -c -d \""
+						+ Utilities.FILE_PATH + "\"";
+				defaultFileEditCommand = "filemgr -c -d \""
+						+ Utilities.FILE_PARENT_PATH + "\"";
+			}
+		}
+	}
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "PathTools";
+
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void initializeDefaultPreferences(IPreferenceStore store) {
+		store.setDefault(FOLDER_EXPLORE_COMMAND_KEY,
+				defaultFolderExploreCommand);
+		store.setDefault(FILE_EXPLORE_COMMAND_KEY, defaultFileExploreCommand);
+		store.setDefault(FOLDER_EDIT_COMMAND_KEY, defaultFolderEditCommand);
+		store.setDefault(FILE_EDIT_COMMAND_KEY, defaultFileEditCommand);
+		super.initializeDefaultPreferences(store);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
---
+++ branches/eclipse3.2.2/PathTools/src/pathtools/ExploreAction.java
@@ -0,0 +1,99 @@
+package pathtools;
+
+import java.io.File;
+import java.text.MessageFormat;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+/**
+ * This launches the OS file explorer showing the selected folder or the folder
+ * containing the selected file.
+ * 
+ * @author Sandip V. Chitale
+ * 
+ */
+public class ExploreAction implements IWorkbenchWindowActionDelegate {
+	private File fileObject;
+
+	private static String fileExploreComand = null;
+	private static String folderExploreComand = null;
+
+	public void dispose() {
+	}
+
+	public void init(IWorkbenchWindow window) {
+
+	}
+
+	public void run(IAction action) {
+		// Get the configured explorer commands for folder and file
+		folderExploreComand = Activator.getDefault().getPreferenceStore()
+				.getString(Activator.FOLDER_EXPLORE_COMMAND_KEY);
+		fileExploreComand = Activator.getDefault().getPreferenceStore()
+				.getString(Activator.FILE_EXPLORE_COMMAND_KEY);
+		if (fileExploreComand == null || folderExploreComand == null) {
+			return;
+		}
+		// Is this a physical file on the disk ?
+		if (fileObject != null) {
+			String commandFormat = fileObject.isDirectory() ? folderExploreComand
+					: fileExploreComand;
+
+			// Substitute parameter values ad format the explore command
+			String command = MessageFormat.format(Utilities
+					.convertParameters(commandFormat), new Object[] {
+					fileObject.getAbsolutePath().replace('/',
+							File.separatorChar).replace('\\',
+							File.separatorChar),
+					fileObject.getParentFile().getAbsolutePath().replace('/',
+							File.separatorChar).replace('\\',
+							File.separatorChar),
+					fileObject.getAbsolutePath().replace('\\', '/'),
+					fileObject.getParentFile().getAbsolutePath().replace('\\',
+							'/'),
+					fileObject.getAbsolutePath().replace('/', '\\'),
+					fileObject.getParentFile().getAbsolutePath().replace('/',
+							'\\'), });
+			// Launch the explore command
+			CommandLauncher.launch(command);
+		}
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		fileObject = null;
+		action.setEnabled(false);
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+			IPath location = null;
+			// Is only one item selected?
+			if (structuredSelection.size() == 1) {
+				Object firstElement = structuredSelection.getFirstElement();
+				if (firstElement instanceof IResource) {
+					// Is this an IResource ?
+					IResource resource = (IResource) firstElement;
+					location = resource.getLocation();
+				} else if (firstElement instanceof IAdaptable) {
+					IAdaptable adaptable = (IAdaptable) firstElement;
+					// Is this an IResource adaptable ?
+					IResource resource = (IResource) adaptable
+							.getAdapter(IResource.class);
+					if (resource != null) {
+						location = resource.getLocation();
+					}
+				}
+			}
+			if (location != null) {
+				fileObject = location.toFile();
+			}
+		}
+		action.setEnabled(fileObject != null);
+	}
+
+}
