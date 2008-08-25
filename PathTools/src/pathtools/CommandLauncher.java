e567ffe07a4194bb014dda97165420b7e186d396
sandipchitale <sandipchitale@fd0683c2-ad54-0410-998d-8999794ab908>
2008-08-25 03:48:15 +0000

    Use org.eclipse.ui.actionSetPartAssociations. Launch external command on a sperate thread.

--- PathTools/src/pathtools/CommandLauncher.java
+++ PathTools/src/pathtools/CommandLauncher.java
@@ -14,39 +14,44 @@ import org.eclipse.core.runtime.Status;
  */
 public class CommandLauncher {
 
-	public static void launch(String command) {
-		Activator activator = Activator.getDefault();
-		String[] commandArray = Utilities.parseParameters(command);
-		try {
-			Process process = Runtime.getRuntime().exec(commandArray);
-			// TODO Should handle STDOUT and STDERR here.
-			int status = process.waitFor();
-			if (status == 0) {
-				// Good
-			} else {
-				activator.getLog().log(
-						new Status(IStatus.ERROR, activator.getBundle()
-								.getSymbolicName(), "Process '"
-								+ Arrays.asList(commandArray).toString()
-								+ "' exited with status: " + status));
-			}
-		} catch (InterruptedException ex) {
-			activator.getLog()
-					.log(
+	public static void launch(final String command) {
+		// Launch command on a separate thread.
+		new Thread(new Runnable() {
+			public void run() {
+				Activator activator = Activator.getDefault();
+				String[] commandArray = Utilities.parseParameters(command);
+				try {
+					Process process = Runtime.getRuntime().exec(commandArray);
+					// TODO Should handle STDOUT and STDERR here.
+					int status = process.waitFor();
+					if (status == 0) {
+						// Good
+					} else {
+						activator.getLog().log(
+								new Status(IStatus.ERROR, activator.getBundle()
+										.getSymbolicName(), "Process '"
+										+ Arrays.asList(commandArray)
+												.toString()
+										+ "' exited with status: " + status));
+					}
+				} catch (InterruptedException ex) {
+					activator.getLog().log(
 							new Status(IStatus.ERROR, activator.getBundle()
 									.getSymbolicName(),
 									"Exception while executing '"
 											+ Arrays.asList(commandArray)
 													.toString() + "'", ex));
-		} catch (IOException ioe) {
-			activator.getLog()
-					.log(
+				} catch (IOException ioe) {
+					activator.getLog().log(
 							new Status(IStatus.ERROR, activator.getBundle()
 									.getSymbolicName(),
 									"Exception while executing '"
 											+ Arrays.asList(commandArray)
 													.toString() + "'", ioe));
-		}
+				}
+
+			}
+		}, "Launching - " + command).start();
 	}
 
 }
