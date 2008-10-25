152ff4b11ba150f28fcf709be5cf486102394635
sandipchitale <sandipchitale@fd0683c2-ad54-0410-998d-8999794ab908>
2008-10-25 18:01:19 +0000

    Add Go to Path drop down to Explore Action dropdown.

--- trunk/PathTools/src/pathtools/ExploreAction.java
+++ trunk/PathTools/src/pathtools/ExploreAction.java
@@ -8,10 +8,24 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.IWorkbenchWindowPulldownDelegate;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * This launches the OS file explorer showing the selected folder or the folder
@@ -20,17 +34,19 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
  * @author Sandip V. Chitale
  * 
  */
-public class ExploreAction implements IWorkbenchWindowActionDelegate {
+public class ExploreAction implements IWorkbenchWindowPulldownDelegate {
 	private File fileObject;
 
 	private static String fileExploreComand = null;
 	private static String folderExploreComand = null;
 
+	private IWorkbenchWindow window;
+
 	public void dispose() {
 	}
 
 	public void init(IWorkbenchWindow window) {
-
+		this.window = window;
 	}
 
 	public void run(IAction action) {
@@ -73,11 +89,13 @@ public class ExploreAction implements IWorkbenchWindowActionDelegate {
 						if (resource != null) {
 							location = resource.getLocation();
 						}
-					} else if (firstElement.getClass().getName().equals("com.aptana.ide.core.ui.io.file.LocalFile")) {
+					} else if (firstElement.getClass().getName().equals(
+							"com.aptana.ide.core.ui.io.file.LocalFile")) {
 						try {
-							Method getFile = firstElement.getClass().getDeclaredMethod("getFile");
+							Method getFile = firstElement.getClass()
+									.getDeclaredMethod("getFile");
 							Object object = getFile.invoke(firstElement);
-							if (object instanceof File){
+							if (object instanceof File) {
 								fileObject = (File) object;
 								return;
 							}
@@ -89,13 +107,61 @@ public class ExploreAction implements IWorkbenchWindowActionDelegate {
 						}
 					}
 				}
-				if (location != null) {	
+				if (location != null) {
 					fileObject = location.toFile();
 				}
 			}
 		} finally {
 			action.setEnabled(fileObject != null);
 		}
+	}
+
+	private Menu exploreMenu;
+
+	public Menu getMenu(Control parent) {
+		if (exploreMenu == null) {
+			exploreMenu = new Menu(parent);
+			MenuItem gotoAction = new MenuItem(exploreMenu, SWT.PUSH);
+			gotoAction.setText("Go to...");
+			gotoAction.addSelectionListener(new SelectionAdapter() {
+				public void widgetSelected(SelectionEvent e) {
+					String defaultValue = "";
+					Clipboard clipboard = new Clipboard(window.getShell().getDisplay());
+					Object contents = clipboard.getContents(TextTransfer.getInstance());
+					if (contents instanceof String) {
+						defaultValue = (String) contents;
+						if (!new File(defaultValue).exists()) {
+							defaultValue = System.getProperty("user.home", ".");
+						}
+					}
+					InputDialog inputDialog = new InputDialog(
+							window.getShell(), "Go to Path", "Path:", defaultValue, null);
+					if (inputDialog.open() == Window.OK) {
+						String path = inputDialog.getValue();
+						File file = new File(path);
+						// Is this a physical file on the disk ?
+						if (file.exists()) {
+							// Get the configured explorer commands for folder
+							// and file
+							String commandFormat = file.isDirectory() ?
+									Activator
+									.getDefault()
+									.getPreferenceStore()
+									.getString(
+											Activator.FOLDER_EXPLORE_COMMAND_KEY)
+									: Activator
+											.getDefault()
+											.getPreferenceStore()
+											.getString(
+													Activator.FILE_EXPLORE_COMMAND_KEY);
+
+							Utilities.launch(commandFormat, file);
+						}
+					}
+				}
+			});
+		}
+		return exploreMenu;
 	}
 
 }
