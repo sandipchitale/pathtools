14d419f961b6c110f742bd6bd96242c42a66785b
sandipchitale <sandipchitale@fd0683c2-ad54-0410-998d-8999794ab908>
2009-08-28 06:32:33 +0000

    Make Command Line Shell action a pull down action.

--- trunk/PathTools/src/pathtools/ShellAction.java
+++ trunk/PathTools/src/pathtools/ShellAction.java
@@ -1,32 +1,57 @@
 package pathtools;
 
 import java.io.File;
+import java.net.URL;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuCreator;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.osgi.service.datalocation.Location;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.events.MenuEvent;
+import org.eclipse.swt.events.MenuListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.texteditor.ITextEditor;
 
-public class ShellAction implements IWorkbenchWindowActionDelegate {
+public class ShellAction implements IObjectActionDelegate, IMenuCreator {
 	private File fileObject;
+	private File projectFileObject;
 
-	private IWorkbenchWindow window;
+	protected IWorkbenchWindow window;
 
 	public void dispose() {
+		if (exploreMenuInFileMenu != null) {
+			exploreMenuInFileMenu.dispose();
+		}
+		if (exploreMenu != null) {
+			exploreMenu.dispose();
+		}
 	}
-
-	public void init(IWorkbenchWindow window) {
-		this.window = window;
+	
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		this.window = targetPart.getSite().getWorkbenchWindow();
+		action.setMenuCreator(this);
 	}
 
 	public void run(IAction action) {
@@ -41,6 +66,7 @@ public class ShellAction implements IWorkbenchWindowActionDelegate {
 		action.setEnabled(false);
 		try {
 			IPath location = null;
+			IPath projectLocation = null;
 			if (selection instanceof IStructuredSelection) {
 				IStructuredSelection structuredSelection = (IStructuredSelection) selection;
 				// Is only one item selected?
@@ -50,6 +76,10 @@ public class ShellAction implements IWorkbenchWindowActionDelegate {
 						// Is this an IResource ?
 						IResource resource = (IResource) firstElement;
 						location = resource.getLocation();
+						if (!(resource instanceof IProject)) {
+							IProject project = resource.getProject();
+							projectLocation = project.getLocation();
+						}
 					} else if (firstElement instanceof IAdaptable) {
 						IAdaptable adaptable = (IAdaptable) firstElement;
 						// Is this an IResource adaptable ?
@@ -57,6 +87,10 @@ public class ShellAction implements IWorkbenchWindowActionDelegate {
 								.getAdapter(IResource.class);
 						if (resource != null) {
 							location = resource.getLocation();
+							if (!(resource instanceof IProject)) {
+								IProject project = resource.getProject();
+								projectLocation = project.getLocation();
+							}
 						}
 					}
 				}
@@ -83,9 +117,174 @@ public class ShellAction implements IWorkbenchWindowActionDelegate {
 			if (location != null) {
 				fileObject = location.toFile();
 			}
+			
+			if (projectLocation != null) {
+				projectFileObject = projectLocation.toFile();
+			}
 		} finally {
 			action.setEnabled(fileObject != null);
 		}
+	}
+	private Menu exploreMenuInFileMenu;
+	public Menu getMenu(Menu parent) {
+		exploreMenuInFileMenu = new Menu(parent);
+		exploreMenuInFileMenu.addMenuListener(new MenuListener() {
+			public void menuHidden(MenuEvent e) {}
+			public void menuShown(MenuEvent e) {
+				MenuItem[] items = exploreMenuInFileMenu.getItems();
+				for (MenuItem menuItem : items) {
+					menuItem.dispose();
+				}
+				fillMenu(exploreMenuInFileMenu);
+			}			
+		});
+		return exploreMenuInFileMenu;
+	}
+	
+	private Menu exploreMenu;
+	public Menu getMenu(Control parent) {
+		if (exploreMenu != null) {
+			exploreMenu.dispose();
+		}
+		exploreMenu = new Menu(parent);
+		fillMenu(exploreMenu);
+		return exploreMenu;
+		
+	}
+
+	private void fillMenu(Menu menu) {
+		if (fileObject != null) {
+			File gotoFile = fileObject;
+			while (gotoFile != null) {
+				final File finalGotoFile = gotoFile;
+				MenuItem gotoParentAction = new MenuItem(menu, SWT.PUSH);
+				gotoParentAction.setText("Command Line Shell at " + gotoFile.getAbsolutePath());
+				gotoParentAction.addSelectionListener(new SelectionAdapter() {
+					public void widgetSelected(SelectionEvent e) {
+						shell(finalGotoFile);
+					}
+				});
+				gotoFile = gotoFile.getParentFile();
+			}
+			new MenuItem(menu, SWT.SEPARATOR);
+		}
+
+		MenuItem gotoAction = new MenuItem(menu, SWT.PUSH);
+		gotoAction.setText("Command Line Shell at...");
+		gotoAction.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				String defaultValue = "";
+				Clipboard clipboard = new Clipboard(window.getShell().getDisplay());
+				Object contents = clipboard.getContents(TextTransfer.getInstance());
+				if (contents instanceof String) {
+					defaultValue = (String) contents;
+					if (!new File(defaultValue).exists()) {
+						defaultValue = System.getProperty("user.home", ".");
+					}
+				}
+				DirectoryDialog directoryDialog = new DirectoryDialog(window.getShell());
+				File defaultDirectory = new File(defaultValue);
+				if (defaultDirectory.exists() && defaultDirectory.isDirectory()) {
+					directoryDialog.setFilterPath(defaultDirectory.getAbsolutePath());
+				}
+				String directory = directoryDialog.open();
+				if (directory != null) {
+					File file = new File(directory);
+					// Is this a physical file on the disk ?
+					if (file.exists()) {
+						// Get the configured explorer commands for folder
+						// and file
+						shell(file);
+					}
+				}
+			}
+		});
+
+		new MenuItem(menu, SWT.SEPARATOR);
+
+		if (projectFileObject != null) {
+			final File finalGotoFile = projectFileObject;
+			MenuItem gotoParentAction = new MenuItem(menu, SWT.PUSH);
+			gotoParentAction.setText("Command Line Shell at Project Folder");
+			gotoParentAction.addSelectionListener(new SelectionAdapter() {
+				public void widgetSelected(SelectionEvent e) {
+					shell(finalGotoFile);
+				}
+			});
+			new MenuItem(menu, SWT.SEPARATOR);
+		}
+
+		final IPath workspaceLocation = ResourcesPlugin.getWorkspace().getRoot().getLocation();
+		MenuItem gotoWorkspace = new MenuItem(menu, SWT.PUSH);
+		gotoWorkspace.setText("Command Line Shell at Workspace Folder: " + workspaceLocation.toFile().getAbsolutePath());
+		gotoWorkspace.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				shell(workspaceLocation.toFile());
+			}
+		});
+
+		Location configurationLocation = Platform.getConfigurationLocation();
+		if (configurationLocation != null) {
+			final URL url = configurationLocation.getURL();
+			if (url != null && new File(url.getFile()).exists()) {
+				MenuItem gotoConfigurationFolder = new MenuItem(menu, SWT.PUSH);
+				gotoConfigurationFolder.setText("Command Line Shell at Configuration Folder: " + url.getFile());
+				gotoConfigurationFolder.addSelectionListener(new SelectionAdapter() {
+					public void widgetSelected(SelectionEvent e) {
+						shell(new File(url.getFile()));
+					}
+				});
+			}
+		}
+
+		Location userDataLocation = Platform.getUserLocation();
+		if (userDataLocation != null) {
+			final URL url = userDataLocation.getURL();
+			if (url != null && (new File(url.getFile()).exists())) {
+				MenuItem gotoUserFolder = new MenuItem(menu, SWT.PUSH);
+				gotoUserFolder.setText("Command Line Shell at User Data Folder: " + url.getFile());
+				gotoUserFolder.addSelectionListener(new SelectionAdapter() {
+					public void widgetSelected(SelectionEvent e) {
+						shell(new File(url.getFile()));
+					}
+				});
+			}
+		}
+		Location installLocation = Platform.getInstallLocation();
+		if (installLocation != null) {
+			final URL url = installLocation.getURL();
+			if (url != null) {
+				MenuItem gotoInstallFolder = new MenuItem(menu, SWT.PUSH);
+				gotoInstallFolder.setText("Command Line Shell at Install Folder: " + url.getFile());
+				gotoInstallFolder.addSelectionListener(new SelectionAdapter() {
+					public void widgetSelected(SelectionEvent e) {
+						shell(new File(url.getFile()));
+					}
+				});
+			}
+		}
+		new MenuItem(menu, SWT.SEPARATOR);
+		MenuItem userHomeFolder = new MenuItem(menu, SWT.PUSH);
+		userHomeFolder.setText("Command Line Shell at user.home: " + System.getProperty("user.home"));
+		userHomeFolder.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {				
+				shell(new File(System.getProperty("user.home")));
+			}
+		});
+		MenuItem userDirFolder = new MenuItem(menu, SWT.PUSH);
+		userDirFolder.setText("Command Line Shell at user.dir: " + System.getProperty("user.dir"));
+		userDirFolder.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				shell(new File(System.getProperty("user.dir")));
+			}
+		});
+		MenuItem javaIoTmpFolder = new MenuItem(menu, SWT.PUSH);
+		javaIoTmpFolder.setText("Command Line Shell at java.io.tmpdir: " + System.getProperty("java.io.tmpdir"));
+		javaIoTmpFolder.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {				
+				shell(new File(System.getProperty("java.io.tmpdir")));
+			}
+		});
 	}
 	
 	private static void shell(File file) {
---
+++ trunk/PathTools/src/pathtools/ShellWindowPulldownAction.java
@@ -0,0 +1,19 @@
+package pathtools;
+
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowPulldownDelegate2;
+
+/**
+ * This launches the OS file explorer showing the selected folder or the folder
+ * containing the selected file.
+ * 
+ * @author Sandip V. Chitale
+ * 
+ */
+public class ShellWindowPulldownAction extends ShellAction implements IWorkbenchWindowPulldownDelegate2 {
+
+	public void init(IWorkbenchWindow window) {
+		this.window = window;
+	}
+
+}
