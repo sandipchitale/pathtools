c4d97df36aefe861e23e74a445aee7580cd9607c
sandipchitale <sandipchitale@fd0683c2-ad54-0410-998d-8999794ab908>
2011-11-29 06:31:53 +0000

    Add support for Explore and Command Line Shell for RSE nodes.

---
+++ PathToolsRSE/src/pathtoolsrse/ExploreHandler.java
@@ -0,0 +1,40 @@
+package pathtoolsrse;
+
+import java.io.File;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.rse.subsystems.files.core.subsystems.IRemoteFile;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import pathtools.ExploreAction;
+
+public class ExploreHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+			if (structuredSelection.size() == 1) {
+					Object firstElement = structuredSelection.getFirstElement();
+					if (firstElement instanceof IRemoteFile) {
+						IRemoteFile remoteFile = (IRemoteFile) firstElement;
+						Object fileObject = remoteFile.getFile();
+						if (fileObject instanceof File) {
+							ExploreAction.explore((File) fileObject);
+						}
+					}
+			} else {
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+				.getShell().getDisplay().beep();
+			}
+		}
+		return null;
+	}
+
+}
